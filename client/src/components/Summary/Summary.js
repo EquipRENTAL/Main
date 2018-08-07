9db3cd68e12cf97a6d985ee829e54926dac4cd42
Janell Guzman <janellguzman@gmail.com>
2018-08-07 15:36:23 -0700

    Updating summary pics.

--- client/src/components/Summary/Summary.js
+++ client/src/components/Summary/Summary.js
@@ -4,7 +4,7 @@ import "./Summary.css"
 const Summary = props =>
     <div id="imagesMain">
                 <img src={'https://raw.githubusercontent.com/EquipRENTAL/Homepage/master/assets/img/gear/campingIcons.jpg'} alt="lets go" />
-                <img src={'https://img.freepik.com/free-vector/cute-camping-equipment-in-flat-design_23-2147541381.jpg?size=338&ext=jpg'} alt="choices" />
+                <img src={'https://raw.githubusercontent.com/EquipRENTAL/Homepage/master/assets/img/parallax/campIcons.png'} alt="choices" />
                 <img src={'https://thumbs.dreamstime.com/b/couple-backpackers-vacation-traveling-47917394.jpg'} alt="campers" />
     </div>
 
