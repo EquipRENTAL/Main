2b2ea64947d8f6b07d1163f9b111e3adaa5b36b9
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-08-07 21:21:58 -0700

    converted Post.js to react, created property states, added to functions to handle typing and clicking submit to POST to database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all items
   getItems: function() {
     return axios.get("http://localhost:3001/api/");
   },
-  // Gets the book with the given id
+  // Gets the item with the given id
   getOne: function(id) {
     return axios.get("http://localhost:3001/api/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the item with the given id
   deleteItems: function(id) {
     return axios.delete("http://localhost:3001/api/" + id);
   },
-  // Saves a book to the database
+  // Saves a item to the database
   saveItems: function(bookData) {
     return axios.post("http://localhost:3001/api/", bookData);
   },
--- models/itemsSchema.js
+++ models/itemsSchema.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-  user: { type: String, required: true },
-  category: {type: String, required: true },
-  title: {type: String, required: true},
-  description: String,
-  image: { type: String, required: true},
-  date: { type: Date, default: Date.now }
+  user: { type: String, required: true, unique: false },
+  category: {type: String, required: true, unique: false },
+  title: {type: String, required: true, unique: false},
+  description: { type: String, unique:false },
+  image: { type: String, required: true, unique:false},
 });
 
 const Items = mongoose.model("Items", itemSchema);
